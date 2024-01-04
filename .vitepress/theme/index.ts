3b203e1f80661ebec4fce8f35bd0939215582b7d
Suyi <thonatos.yang@gmail.com>
2024-01-04 15:42:47 +0800

    chore: support image zoom (#33)

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue';
-import type { Theme } from 'vitepress';
+import { h, nextTick, watch } from 'vue';
+import { inBrowser, useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
 import DefaultTheme from 'vitepress/theme';
+import type { Theme } from 'vitepress';
 import './style.css';
 
 export default {
@@ -13,5 +15,25 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     // ...
+  },
+  setup() {
+    const route = useRoute();
+    watch(
+      () => route.path,
+      () =>
+        nextTick(() => {
+          if (inBrowser)
+            document.querySelectorAll('.vp-doc img').forEach((item) => {
+              item.setAttribute('data-zoomable', '');
+              item.setAttribute('class', 'medium-zoom-image');
+            });
+
+          mediumZoom('[data-zoomable]', {
+            margin: 32,
+            background: 'var(--vp-c-bg)',
+          });
+        }),
+      { immediate: true }
+    );
   },
 } satisfies Theme;
