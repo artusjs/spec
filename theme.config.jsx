12efd1044d3e3144f00f598d3bde0e5e036f465d
Suyi <thonatos.yang@gmail.com>
2024-02-19 21:18:54 +0800

    chore(docs): support og for twitter (#40)

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
 
 const config = {
   useNextSeoProps() {
@@ -30,11 +31,46 @@ const config = {
   toc: {
     backToTop: true,
   },
-  head: (
-    <>
-      <link rel='icon' href='/logo/nodejs-logo.svg' type='image/svg+xml' />
-    </>
-  ),
+  head: function useHead() {
+    const { title } = useConfig();
+    const { route } = useRouter();
+
+    const socialCard =
+      route === '/' || !title
+        ? 'https://artusjs.org/og.png'
+        : `https://artusjs.org/api/og?title=${title}`;
+
+    return (
+      <>
+        <meta name='msapplication-TileColor' content='#fff' />
+        <meta name='theme-color' content='#fff' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv='Content-Language' content='en' />
+        <meta
+          name='description'
+          content='Enterprise Node.js Framework for Web, RPC, Serverless, etc.'
+        />
+        <meta
+          name='og:description'
+          content='Enterprise Node.js Framework for Web, RPC, Serverless, etc.'
+        />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:image' content={socialCard} />
+        <meta name='twitter:site:domain' content='artusjs.org' />
+        <meta name='twitter:url' content='https://artusjs.org' />
+        <meta name='og:title' content={title ? title + ' – Artus' : 'Artus'} />
+        <meta name='og:image' content={socialCard} />
+        <meta name='apple-mobile-web-app-title' content='Artus' />
+        <link rel='icon' href='/logo/nodejs-logo.svg' type='image/svg+xml' />
+        <link
+          rel='icon'
+          href='/logo/nodejs-logo.svg'
+          type='image/svg+xml'
+          media='(prefers-color-scheme: dark)'
+        />
+      </>
+    );
+  },
   footer: {
     text: (
       <div className='nx-flex nx-flex nx-flex-col nx-gap-2'>
