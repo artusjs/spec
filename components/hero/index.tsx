6a8e2a5d97105619aa7637a6837fdc6c0f165707
Suyi <thonatos.yang@gmail.com>
2024-02-19 12:41:58 +0000

    chore(docs): support og for twitter

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -10,19 +10,26 @@ const Hero: React.FC<{}> = () => {
 
   return (
     <div className='content-container'>
-      <h1 className={styles.headline}>
-        {data.title} <br /> {data.subtitle}
-      </h1>
-      <p className={styles.subtitle}>{data.tagline}</p>
-      <p className={styles.subtitle}>
-        {(data.actions || []).map((item, index) => {
-          return (
-            <Link className={styles.cta} href={item.link} key={index}>
-              {item.text}
-            </Link>
-          );
-        })}
-      </p>
+      <div className='flex flex-col-reverse gap-4 md:flex-row md:justify-between md:items-center'>
+        <div className='hero-intro flex-none md:w-3/5'>
+          <h1 className={styles.headline}>
+            {data.title} <br /> {data.subtitle}
+          </h1>
+          <p className={styles.subtitle}>{data.tagline}</p>
+          <p className={styles.subtitle}>
+            {(data.actions || []).map((item, index) => {
+              return (
+                <Link className={styles.cta} href={item.link} key={index}>
+                  {item.text}
+                </Link>
+              );
+            })}
+          </p>
+        </div>
+        <div className='hero-banner grow'>
+          <img className='w-full md:max-w-96' src={data.image.src} alt={data.image.alt} />
+        </div>
+      </div>
     </div>
   );
 };
