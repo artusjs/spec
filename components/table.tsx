a226eb2d23ff8d67e533365e18db05c1e0db676c
Suyi <thonatos.yang@gmail.com>
2024-05-12 13:55:01 +0800

    feat: update table component

--- components/table.tsx
+++ components/table.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
+import Table from 'rc-table';
 import { useConfig } from 'nextra-theme-docs';
 
-const Table: React.FC<{}> = () => {
+import 'rc-table/assets/index.css';
+
+const Packages: React.FC<{}> = () => {
   const config = useConfig();
   const packages = config.frontMatter.packages;
 
+  const columns = [
+    {
+      title: 'Package',
+      dataIndex: 'package',
+      key: 'package',
+    },
+    {
+      title: 'Version',
+      dataIndex: 'version',
+      key: 'version',
+    },
+  ];
+
+  const data = packages.map((item) => ({
+    package: <a href={item.url}>{item.name}</a>,
+    version: <img src={item.badge} alt={item.name} />,
+  }));
+
   return (
-    <div className='content-container'>
-      <div className=''>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Version</th>
-            </tr>
-          </thead>
-          <tbody>
-            {packages.map((item) => (
-              <tr key={item.name}>
-                <td>
-                  <a href={item.url}>{item.name}</a>
-                </td>
-                <td>
-                  <img src={item.badge} alt={item.name} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+    <div className='container py-4'>
+      <Table tableLayout='auto' columns={columns} data={data} />
     </div>
   );
 };
 
-export default Table;
+export default Packages;
